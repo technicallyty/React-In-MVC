446ea36064aa3f52a80855aadb5681b7bbed3205
tytech3 <48813565+tytech3@users.noreply.github.com>
2020-09-17 17:07:33 -0700

    Add project files.

---
+++ Scripts/Home/react/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <React.Fragment>
+        <h1>This is a react function</h1>
+        <div>This is rendered via react</div>
+    </React.Fragment>
+);
+
+render(<App />, document.getElementById('app'));
